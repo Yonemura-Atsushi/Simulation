92c31bc976d00e5b8d6f96653fb78110a598be4e
a-yonemura <yoneatsu1121@gmail.com>
2023-11-18 01:11:38 +0900

    20231118 米村 サーバー起動～初期画面表示

--- StartUp/StartUp.py
+++ StartUp/StartUp.py
@@ -1,24 +1,34 @@
-# ライブラリ読み込み
-from selenium import webdriver
+import threading,webbrowser
 from flask import Flask
-from multiprocessing import Process
-import time
-
-# flask
 app = Flask(__name__, static_folder='.', static_url_path='')
 
-# ページ表示
+# 初期画面表示
 @app.route('/')
-def index():
+def mainMenu():
     return app.send_static_file('AMainMenu.html')
 
-# webブラウザ起動
-def runChrome():
-    driver = webdriver.Chrome()
-    driver.get("http://localhost:8080/")
+# # 等速・等加速度直線画面表示
+# @app.route('/LinearMotion')
+# def linearMotion():
+#     return app.send_static_file('Simulator\LinearMotion\LinearMotion.html')
 
-def run():
-    app.run(port = 8080, debug = False)
+# # 鉛直投げ上げ画面表示
+# @app.route('/VerticalThrow')
+# def verticalThrow():
+#     return app.send_static_file('Simulator\VerticalThrow\VerticalThrow.html')
 
-if __name__ == '__main__':
-    app.run(port = 8080, debug = False)
+# # 斜方投射画面表示
+# @app.route('/ObliqueProjection')
+# def obliqueProjection():
+#     return app.send_static_file('Simulator\ObliqueProjection\ObliqueProjection.html')
+
+# # 自由落下画面表示
+# @app.route('/FreeFall')
+# def freeFall():
+#     return app.send_static_file('Simulator\FreeFall\FreeFall.html')
+
+if __name__ == "__main__":
+    # ウィンドウオープン
+    threading.Timer(1.0, lambda: webbrowser.open('http://localhost:8080') ).start()
+    # サーバ起動
+    app.run(port=8080, debug=False)
