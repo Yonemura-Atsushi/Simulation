10505b1fb4a11d616baf922c8432c2e5faf2f94a
a-yonemura <yoneatsu1121@gmail.com>
2023-11-19 21:21:53 +0900

    20231119 不要処理削除

--- StartUp/app.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask
-app = Flask(__name__, static_folder='.', static_url_path='')
-@app.route('/')
-def index():
-    return app.send_static_file('AMainMenu.html')
-
-app.run(port=8080, debug=False)
