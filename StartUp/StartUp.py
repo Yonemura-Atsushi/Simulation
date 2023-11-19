f557847169608a015209415324de536f6620a167
a-yonemura <yoneatsu1121@gmail.com>
2023-11-19 21:14:31 +0900

    20231119 ハイパーリンク作成

--- StartUp/StartUp.py
+++ StartUp/StartUp.py
@@ -7,25 +7,25 @@ app = Flask(__name__, static_folder='.', static_url_path='')
 def mainMenu():
     return app.send_static_file('AMainMenu.html')
 
-# # 等速・等加速度直線画面表示
-# @app.route('/LinearMotion')
-# def linearMotion():
-#     return app.send_static_file('Simulator\LinearMotion\LinearMotion.html')
+# 等速・等加速度直線画面表示
+@app.route('/LinearMotion')
+def linearMotion():
+    return app.send_static_file('Simulator\LinearMotion\LinearMotion.html')
 
-# # 鉛直投げ上げ画面表示
-# @app.route('/VerticalThrow')
-# def verticalThrow():
-#     return app.send_static_file('Simulator\VerticalThrow\VerticalThrow.html')
+# 鉛直投げ上げ画面表示
+@app.route('/VerticalThrow')
+def verticalThrow():
+    return app.send_static_file('Simulator\VerticalThrow\VerticalThrow.html')
 
-# # 斜方投射画面表示
-# @app.route('/ObliqueProjection')
-# def obliqueProjection():
-#     return app.send_static_file('Simulator\ObliqueProjection\ObliqueProjection.html')
+# 斜方投射画面表示
+@app.route('/ObliqueProjection')
+def obliqueProjection():
+    return app.send_static_file('Simulator\ObliqueProjection\ObliqueProjection.html')
 
-# # 自由落下画面表示
-# @app.route('/FreeFall')
-# def freeFall():
-#     return app.send_static_file('Simulator\FreeFall\FreeFall.html')
+# 自由落下画面表示
+@app.route('/FreeFall')
+def freeFall():
+    return app.send_static_file('Simulator\FreeFall\FreeFall.html')
 
 if __name__ == "__main__":
     # ウィンドウオープン
